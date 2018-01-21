490126a8d327acc37d3df50619df43b91475da35
Clement <clement.hoang24@gmail.com>
2018-01-21 18:59:40 -0500

    Add a2 base

---
+++ a2/q2.cc
@@ -0,0 +1,32 @@
+class Potato {
+    // YOU ADD MEMBERS HERE
+  public:
+    _Event Explode {};
+    Potato( unsigned int maxTicks = 10 );
+    void reset( unsigned int maxTicks = 10 );
+    void countdown();
+};
+
+_Coroutine Player {
+    _Event Terminate {
+      public:
+        Player * victim;                      // delete player
+        Terminate( Player * victim ) : victim( victim ) {}
+    };
+    _Event Election {
+      public:
+        int id;                               // highest id seen so far
+        Election( int id ) : id( id ) {}
+    };
+    Player * partner[2];                      // left and right player
+    // YOU ADD MEMBERS HERE
+    void main();
+  public:
+    static Player * umpire;                   // current umpire
+
+    Player( int id, Potato & potato );
+    void start( Player & lp, Player & rp );   // supply partners
+    void toss();                              // tossed the potato
+    void vote( int id );                      // resume partner to vote
+    void terminate();                         // resume umpire to delete
+};
