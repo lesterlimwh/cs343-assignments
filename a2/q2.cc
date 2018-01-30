3b61aadbb7b4c329bf0f87bb7bfc2c9afcc2ce67
Clement <clement.hoang24@gmail.com>
2018-01-30 02:54:56 -0500

    Add initial hotpotato CLI parsing code

--- a2/q2.cc
+++ a2/q2.cc
@@ -1,32 +1,68 @@
-class Potato {
-    // YOU ADD MEMBERS HERE
-  public:
-    _Event Explode {};
-    Potato( unsigned int maxTicks = 10 );
-    void reset( unsigned int maxTicks = 10 );
-    void countdown();
-};
+#include "q2.h"
 
-_Coroutine Player {
-    _Event Terminate {
-      public:
-        Player * victim;                      // delete player
-        Terminate( Player * victim ) : victim( victim ) {}
-    };
-    _Event Election {
-      public:
-        int id;                               // highest id seen so far
-        Election( int id ) : id( id ) {}
-    };
-    Player * partner[2];                      // left and right player
-    // YOU ADD MEMBERS HERE
-    void main();
-  public:
-    static Player * umpire;                   // current umpire
+using namespace std;
 
-    Player( int id, Potato & potato );
-    void start( Player & lp, Player & rp );   // supply partners
-    void toss();                              // tossed the potato
-    void vote( int id );                      // resume partner to vote
-    void terminate();                         // resume umpire to delete
-};
+PRNG prng; // globally defined prng
+
+// prints usage information
+void usage(char *arg) {
+  cerr << "Usage: " << arg
+    << " [ games (>= 0) | \"x\" [ players (>= 2) | "
+    << "\"x\" [ random-seed (> 0) | \"x\" ] ] ]" << endl;
+  exit(EXIT_FAILURE);
+}
+
+// parses string to a number, and exits program if it fails
+int tryParseStringToNumber(char* arg) {
+  char* endptr;
+
+  long val = strtol(arg, &endptr, 10);
+
+  if (*endptr == 0) { // successfully parsed string
+    return int(val);
+  } else {
+    usage(arg); 
+    return -1; // will never get here, but gets rid of compiler warning
+  }
+}
+
+int main( int argc, char *argv[] ) {
+  int numGames = 5;
+  int numPlayers;
+
+  // parse user input
+  switch (argc) {
+    case 4: // read seed
+      if (argv[3][0] == 'x') {
+        prng.seed(getpid()); // default
+      } else {
+        prng.seed(tryParseStringToNumber(argv[3]));
+      }
+    case 3: // read players
+      if (argv[2][0] == 'x') {
+        numPlayers = prng(2, 10); 
+      } else {
+        numPlayers = tryParseStringToNumber(argv[2]);
+        if (numPlayers < 2) {
+          usage(argv[0]);
+        }
+      }
+    case 2: // read games
+      if (argv[1][0] == 'x') {
+        numGames = 5; 
+      } else {
+        numGames = tryParseStringToNumber(argv[1]);
+        if (numGames < 0) {
+          usage(argv[0]);
+        }
+      }
+    case 1:
+      break;
+    default:
+      usage(argv[0]);
+  }
+
+  if (argc < 3) { // generate numPlayers if it isn't specified in args
+    numPlayers = prng(2, 10); 
+  }
+}
---
+++ a2/q2potato.cc
@@ -0,0 +1,15 @@
+#include "q2.h"
+
+using namespace std;
+
+Potato::Potato(unsigned int maxTicks) {
+  cout << "potato created" << endl;
+}
+
+void Potato::reset(unsigned int maxTicks) {
+  cout << "resetting potato" << endl;
+}
+
+void Potato::countdown() {
+  cout << "potato countdown!" << endl;
+}
