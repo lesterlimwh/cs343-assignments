63e2971b3e0ed4c2d7b861f350b218f536c9e652
Clement <clement.hoang24@gmail.com>
2018-03-07 21:10:55 -0500

    Implement voter main

--- a4/q1voter.cc
+++ a4/q1voter.cc
@@ -4,7 +4,11 @@
 Voter::Voter( unsigned int id, unsigned int nvotes, TallyVotes & voteTallier, Printer & printer ) : voteTallier(voteTallier), printer(printer), id(id), num_votes(nvotes) {}
 
 void Voter::main() {
+  yield(mprng(19)); // yield 0 - 19 times inclusive
   printer.print(id, States::Start);
+  yield(mprng(4)); // yield 0 - 4 times inclusive
+  // vote
+  yield(mprng(4)); // yield 0 - 4 times inclusive
   printer.print(id, States::Finished);
 }
 
